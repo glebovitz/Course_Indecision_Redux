84336992d43356b3fdd38688cc73b9e9a8fc0784
Gregg Lebovitz <gregg@lebovitz.net>
2018-05-23 13:21:43 -0400

    fixes to clear input after change to redux

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => (
-  <Modal
-    isOpen={!!props.selectedOption}
-    onRequestClose={() => props.clearSelectedOption()}
-    contentLabel="Selected Option"
-    closeTimeoutMS={200}
-    className='modal'
-  >
-    <h3 className='modal__title' >Select Option</h3>
-    {props.selectedOption && <p className='model__body' >{props.selectedOption}</p>}
-    <button  className='button' onClick={() => props.clearSelectedOption()}>
-      Okay
-    </button>
-  </Modal>
-);
+export default class OptionModal extends React.Component {
+  state = {
+    lastSelectedOption: ''
+  };
 
-export default OptionModal;
+  // Sequence: 
+  //    1. props.selectedOption is cleared. 
+  //    2. Modal is content updated.
+  //    3. Modal then closes.
+  // This causes the dialog button to jump upward.
+  // Instead store selected option in a local state variabe and only change
+  // when we have a new non empty selected option.
+  static getDerivedStateFromProps = (nextProps, prevState) => {
+    if (nextProps.selectedOption) {
+        return { 
+            lastSelectedOption: nextProps.selectedOption
+        };
+    } else {
+        return null;
+    }
+  };
+
+  render() {
+    return (
+      <Modal
+        isOpen={!!this.props.selectedOption}
+        onRequestClose={() => this.props.clearSelectedOption()}
+        contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className='modal'
+        ariaHideApp={false}
+      >
+        <h3 className='modal__title' >Select Option</h3>
+        {this.state.lastSelectedOption && 
+          <p className='model__body' >{this.state.lastSelectedOption}</p>}
+        <button  className='button' onClick={() => this.props.clearSelectedOption()}>
+          Okay
+        </button>
+      </Modal>
+    );
+  };
+};
